a2059ed6078b2ae09a3df20e3844ca7c24c60bb2
rovy-saputra-nugeraha <2101020012@student.umrah.ac.id>
2023-12-05 20:58:07 +0700

    Navbar Update

--- model/layout/navbar.php
+++ model/layout/navbar.php
@@ -16,15 +16,18 @@
   </style>
 </head>
 <body>
-	<div class="navbar-header">
-		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">         
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-			<a class="navbar-brand" href="home">Absensi Pegawai Universitas Maritim Raja Ali Haji</a>
-	</div>
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">      
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="home">
+       Absensi Pegawai UMRAH
+    </a>
+</div>
+
 	<div id="navbar" class="navbar-collapse collapse">
 		<ul class="nav navbar-nav navbar-right visible-xs visible-sm">
 		<li id="output_m"></li>
--- model/layout/navbars.php
+++ model/layout/navbars.php
@@ -5,7 +5,7 @@
   <span class="icon-bar"></span>
   <span class="icon-bar"></span>
 </button>
-  <a class="navbar-brand" href="home">Absensi Pegawai Universitas Maritim Raja Ali Haji</a>
+  <a class="navbar-brand" href="home">Absensi Pegawai UMRAH</a>
   </div>
 <div id="navbar" class="navbar-collapse collapse">
 <ul class="nav navbar-nav navbar-right visible-xs visible-sm">
