b632887d43311aa602b44dba8ac41e02c1fb2dcf
rovy-saputra-nugeraha <2101020012@student.umrah.ac.id>
2023-12-09 09:26:11 +0700

    Update Konfig

--- lib/db/dbconfig.php
+++ lib/db/dbconfig.php
@@ -3,7 +3,7 @@
     $dbhost = "localhost";
     $dbuser = "root";
     $dbpwd = "";
-    $dbname = "absensi_rampa1";
+    $dbname = "absensi_pegawai";
 
     $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
  ?>
